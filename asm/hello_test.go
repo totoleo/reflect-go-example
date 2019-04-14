0c883aa2ce8981e49c0a70ebbc955d8650fd6565
zhaixiaolei <zhaixiaolei@bytedance.com>
2019-04-14 14:32:01 +0800

    指针操作
    Change-Id: I8e1ad140ed42029b4a3e1a10a89f5a193928c103

--- asm/hello_test.go
+++ asm/hello_test.go
@@ -4,8 +4,10 @@ import "testing"
 
 func TestMake(t *testing.T) {
 	var a int64 = 1
-	var b int64 = 2
+	var b int64 = 4
 
 	ret := makeFuncStub(a, b)
+	t.Log(ret)
+	ret = fooWrap(5, -3)
 	t.Log(ret)
 }
